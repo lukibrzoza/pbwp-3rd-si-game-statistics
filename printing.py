3df62d5879589794dba6aae09d6dc6f837c3a0b3
lukibrzoza <lukibroza@gmail.com>
2018-02-11 21:09:14 +0100

    Done

--- printing.py
+++ printing.py
@@ -1,2 +1,49 @@
 
 # Printing functions
+from reports import *
+while True:
+    menu_select = input('Please select which question do you want to be answered:\n\
+    1. How many games are in the file?\n\
+    2. Is there a game from a given year?\n\
+    3. Which was the latest game?\n\
+    4. How many games do we have by genre?\n\
+    5. What is the line number of the given game (by title)?\n')
+    
+    if menu_select == '1':
+        try:
+            print('Answer: ' + str(count_games(input('Please enter the name of the file: '))) + '\n')
+        except FileNotFoundError:
+            print('\nPlease enter a correct file name.\n')
+            continue
+    
+    elif menu_select == '2':
+        try:
+            print('Answer: ' + str(decide(input('Please enter the name of the file: '), input('Please enter a year: '))) + '\n')
+        except FileNotFoundError:
+            print('\nPlease enter a correct file name.\n')
+            continue
+
+    elif menu_select == '3':
+        try:
+            print('Answer: ' + str(get_latest(input('Please enter the name of the file: '))) + '\n')
+        except FileNotFoundError:
+            print('\nPlease enter a correct file name.\n')
+            continue
+
+    elif menu_select == '4':
+        try:
+            print('Answer: ' + str(count_by_genre(input('Please enter the name of the file: ')), input('Please enter a genre: ')) + '\n')
+        except FileNotFoundError:
+            print('\nPlease enter a correct file name.\n')
+            continue
+
+    elif menu_select == '5':
+        try:
+            print('Answer: ' + str(get_line_number_by_title(input('Please enter the name of the file: ')), input('Please enter game title: ')) + '\n')
+        except FileNotFoundError:
+            print('\nPlease enter a correct file name.\n')
+            continue
+
+    else:
+        print('\nPlease enter a valid number.\n')
+        continue
--- reports.py
+++ reports.py
@@ -1,2 +1,73 @@
 
 # Report functions
+def count_games(file_name):
+    f = open(file_name, 'r')
+    f = f.readlines()
+    return len(f)
+
+def decide(file_name, year):
+    boolean = False
+    f = open(file_name, 'r')
+    f = f.readlines()
+    for arg in f:
+        temp = arg.replace('\t', 'SPLIT')
+        temp = temp.replace('\n', '').split('SPLIT')
+        if int(temp[2]) == int(year):
+            return True
+            break
+        else:
+            continue
+
+    return boolean
+
+def get_latest(file_name):
+    temp_list = []
+    f = open(file_name, 'r')
+    f = f.readlines()
+    for arg in f:
+        temp = arg.replace('\t', 'SPLIT')
+        temp = temp.replace('\n', '').split('SPLIT')
+        try:
+            if int(temp[2]) > int(temp_list[2]):
+                temp_list = temp
+        except:
+            temp_list = temp
+            continue
+
+    return str(temp_list[0])
+
+def count_by_genre(file_name, genre):
+    count = 0
+    f = open(file_name, 'r')
+    f = f.readlines()
+    for arg in f:
+        temp = arg.replace('\t', 'SPLIT')
+        temp = temp.replace('\n', '').split('SPLIT')
+        if str(temp[3]) == genre:
+            count += 1
+        else:
+            continue
+        
+    return count
+
+def get_line_number_by_title(file_name, title):
+    count = 1
+    f = open(file_name, 'r')
+    f = f.readlines()
+    for arg in f:
+        temp = arg.replace('\t', 'SPLIT')
+        temp = temp.replace('\n', '').split('SPLIT')
+        if str(temp[0]) != title:
+            count += 1
+            continue
+        else:
+            print(count)
+            return count
+            break
+
+
+# count_games('game_stat.txt')
+# decide('game_stat.txt',1999)
+# get_latest('game_stat.txt')
+# count_by_genre('game_stat.txt', 'Real-time strategy')
+# get_line_number_by_title('game_stat.txt', 'Warcraft III: Reign of Chaos')
